c8ff34dfd4654966b47d666f5cd3614a75f12194
callmeaf <alikhoshkar2000@gmail.com>
2025-05-11 16:00:36 +0330

    change api resource

--- src/App/Http/Resources/Api/V1/SocialBotResource.php
+++ src/App/Http/Resources/Api/V1/SocialBotResource.php
@@ -20,9 +20,21 @@ class SocialBotResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'social_id' => $this->social_id,
+            'status' => $this->status,
+            'status_text' => $this->statusText,
+            'type' => $this->type,
+            'type_text' => $this->typeText,
+            'name' => $this->name,
+//            'token' => $this->token,
+            'footer' => $this->footer,
+            'footer_text' => $this->footerText,
             'created_at' => $this->created_at,
+            'created_at_text' => $this->createdAtText(),
             'updated_at' => $this->updated_at,
+            'updated_at_text' => $this->updatedAtText(),
             'deleted_at' => $this->deleted_at,
+            'deleted_at_text' => $this->deletedAtText(),
         ];
     }
 }
